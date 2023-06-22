b97f8eaabb9a0febe063e07b9c0df50d7a05bd02
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-22 16:50:14 +0200

    dsa

--- image_cropper.py
+++ image_cropper.py
@@ -1,10 +1,15 @@
 from PIL import Image
 import os
 
-names = os.listdir("./Masks")
+names = os.listdir("./Images")
 
-max_res = 0
-full_res = (0, 0)
+for name in names:
+    filename = os.path.join(f"./Images/{name}")
+    img = Image.open(filename)
+    img = img.resize((1024, 1024))
+    img.save(f"./Images/{name}")
+
+names = os.listdir("./Masks")
 
 for name in names:
     filename = os.path.join(f"./Masks/{name}")
